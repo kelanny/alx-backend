92b4773d2ea3aa58819f9f6fde7e2d2d58d343ce
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-08-24 01:06:13 +0300

    Correcting Task: 5. LFU Caching for bugs

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python3
-""" LFU Caching
-"""
-
+""" LFUCache module """
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ LFU cache replacement algorithm
+    """ LFUCache class that inherits from BaseCaching
+    Implements Least Frequently Used (LFU) caching system.
     """
 
     def __init__(self):
-        """ Init
-        """
+        """Initialize the LFUCache"""
         super().__init__()
-        self.cache = {}
-        self.lfu = {}
-        self.lfu_count = {}
+        self.freq = {}  # Frequency counter
+        self.usage = {}  # Tracks the order of insertion and usage
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        """Assign item value to key in cache with LFU algorithm"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            min_lfu = min(self.lfu_count.values())
-            keys = [k for k in self.lfu_count if self.lfu_count[k] == min_lfu]
-            if len(keys) == 1:
-                del self.cache_data[keys[0]]
-                del self.lfu[keys[0]]
-                del self.lfu_count[keys[0]]
-            else:
-                min_lfu = min(self.lfu[keys[0]], self.lfu[keys[1]])
-                key = keys[0] if self.lfu[keys[0]] == min_lfu else keys[1]
-                del self.cache_data[key]
-                del self.lfu[key]
-                del self.lfu_count[key]
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.freq[key] += 1
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Find the least frequently used items
+                min_freq = min(self.freq.values())
+                candidates = [k for k, v in self.freq.items() if v == min_freq]
+                # If there are ties, use LRU
+                if len(candidates) > 1:
+                    oldest = min(candidates, key=lambda k: self.usage[k])
+                    del self.cache_data[oldest]
+                    del self.freq[oldest]
+                    del self.usage[oldest]
+                    print(f"DISCARD: {oldest}")
+                else:
+                    discard = candidates[0]
+                    del self.cache_data[discard]
+                    del self.freq[discard]
+                    del self.usage[discard]
+                    print(f"DISCARD: {discard}")
 
-        self.cache_data[key] = item
-        self.lfu[key] = 0
-        self.lfu_count[key] = 0
+            self.cache_data[key] = item
+            self.freq[key] = 1
+
+        # Update usage to track LRU in case of ties
+        self.usage[key] = len(self.usage)
 
     def get(self, key):
-        """ Get an item by key
-        """
+        """Return the value in cache linked to key"""
         if key is None or key not in self.cache_data:
             return None
 
-        self.lfu[key] += 1
-        self.lfu_count[key] += 1
+        self.freq[key] += 1
+        # Update usage to mark as recently used
+        self.usage[key] = len(self.usage)
         return self.cache_data[key]
